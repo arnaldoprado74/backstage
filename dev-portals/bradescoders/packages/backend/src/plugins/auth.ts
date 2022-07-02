345253e9a043cc8f4010ff6caf47342c25ffc497
arnaldoprado74 <arnaldo.prado@gmail.com>
2022-07-02 13:06:35 -0300

    gitlab resolver v atual

--- dev-portals/bradescoders/packages/backend/src/plugins/auth.ts
+++ dev-portals/bradescoders/packages/backend/src/plugins/auth.ts
@@ -51,15 +51,15 @@ export default async function createPlugin(
       }),
       gitlab: providers.gitlab.create({
         signIn: {
-          resolver(_, ctx) {
-            const userRef = 'user:default/guest'; // Must be a full entity reference
-            return ctx.issueToken({
-              claims: {
-                sub: userRef, // The user's own identity
-                ent: [userRef], // A list of identities that the user claims ownership through
-              },
-            });
-          },
+          resolver(info, ctx) {
+            const { result } = info;
+
+            const id = result.fullProfile.username;
+            if (!id) {
+              throw new Error(`GitLab user profile does not contain a username`);
+            }
+
+            return ctx.signInWithCatalogUser({ entityRef: { name: id } });
         },
       }),
     },
