bcc0ea3f515600071c26b674627cc35c6da80187
arnaldoprado74 <arnaldo.prado@gmail.com>
2022-07-02 17:33:41 -0300

    syntax corretion'

--- dev-portals/bradescoders/packages/backend/src/plugins/auth.ts
+++ dev-portals/bradescoders/packages/backend/src/plugins/auth.ts
@@ -60,6 +60,7 @@ export default async function createPlugin(
             }
 
             return ctx.signInWithCatalogUser({ entityRef: { name: id } });
+          },
         },
       }),
     },
